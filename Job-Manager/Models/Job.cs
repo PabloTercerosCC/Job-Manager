5380458adab797651ebdc3105058bc21999ebb81
PabloTercerosCC <pablo.terceros.60104@usalesiana.edu.bo>
2023-10-13 22:32:56 -0400

    initial commit

---
+++ Job-Manager/Models/Job.cs
@@ -0,0 +1,18 @@
+﻿namespace Job_Manager.Models
+{
+    public class Job
+    {
+        public string id { get; set; }
+
+        public string text { get; set; }
+
+        public string date { get; set; }
+
+        public bool reminder { get; set; }
+
+        public Job()
+        {
+            
+        }
+    }
+}
